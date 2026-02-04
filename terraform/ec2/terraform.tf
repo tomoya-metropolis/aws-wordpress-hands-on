2e2ea86cc62aff4042c61ebc9696a826c0bfd93e
Tomoya Sasaki <tomoya.let.it.bleed@gmail.com>
2026-02-04 21:06:16 +0900

    Add lines to launch EC2 instances

--- terraform/ec2/terraform.tf
+++ terraform/ec2/terraform.tf
@@ -46,3 +46,49 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = "-1"
 }
+
+data "aws_ami" "wordpress-ami" {
+  most_recent = true
+  name_regex  = "wordpress-*"
+  owners      = ["self"]
+}
+
+data "aws_subnet" "public-1a" {
+  filter {
+    name   = "tag:Name"
+    values = ["public-1a"]
+  }
+}
+
+data "aws_subnet" "public-1c" {
+  filter {
+    name   = "tag:Name"
+    values = ["public-1c"]
+  }
+}
+
+resource "aws_instance" "wordpress-1a" {
+  ami                         = data.aws_ami.wordpress-ami.id
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnet.public-1a.id
+  associate_public_ip_address = "true"
+  key_name                    = "ec2"
+  vpc_security_group_ids      = [aws_security_group.worpress-security-groups.id]
+
+  tags = {
+    Name = "wordpress-1a"
+  }
+}
+
+resource "aws_instance" "wordpress-1c" {
+  ami                         = data.aws_ami.wordpress-ami.id
+  instance_type               = "t3.micro"
+  subnet_id                   = data.aws_subnet.public-1c.id
+  associate_public_ip_address = "true"
+  key_name                    = "ec2"
+  vpc_security_group_ids      = [aws_security_group.worpress-security-groups.id]
+
+  tags = {
+    Name = "wordpress-1c"
+  }
+}
