e3af349b84da0961695469b38c4d230a5c7d4e99
Tomoya Sasaki <t.sasaki@future-shop.co.jp>
2026-02-12 18:34:16 +0900

    Modify security group resources

--- terraform/rds/terraform.tf
+++ terraform/rds/terraform.tf
@@ -36,7 +36,7 @@ resource "aws_security_group" "wordpress-rds-security-group" {
   }
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+resource "aws_vpc_security_group_ingress_rule" "allow_mysql" {
   security_group_id            = aws_security_group.wordpress-rds-security-group.id
   from_port                    = 3306
   ip_protocol                  = "tcp"
@@ -44,7 +44,7 @@ resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
   referenced_security_group_id = data.aws_security_group.wordpress-security-group.id
 }
 
-resource "aws_vpc_security_group_egress_rule" "allow_http" {
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
   security_group_id = aws_security_group.wordpress-rds-security-group.id
   cidr_ipv4         = "0.0.0.0/0"
   ip_protocol       = -1
@@ -86,4 +86,5 @@ resource "aws_db_instance" "wordpress-rds" {
   skip_final_snapshot  = true
   multi_az             = true
   db_subnet_group_name = "wordpress-db-subnet-group"
+  vpc_security_group_ids  = [aws_security_group.wordpress-rds-security-group.id]
 }
